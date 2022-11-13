61832f4e4abf3ad404021de420904ea45f4f5ab2
Jiang <wj3972@lib-pclem113.lib.utexas.edu>
2022-11-13 17:56:16 -0600

    comment out firebase

--- BashGate/RegisterVC.swift
+++ BashGate/RegisterVC.swift
@@ -1,63 +1,63 @@
+////
+////  RegisterVC.swift
+////  BashGate
+////
+////  Created by Saahir Janmohamed on 10/12/22.
+////
 //
-//  RegisterVC.swift
-//  BashGate
+//import UIKit
+//import FirebaseAuth
 //
-//  Created by Saahir Janmohamed on 10/12/22.
+//class RegisterVC: UIViewController {
+//    @IBOutlet weak var emailText: UITextField!
+//    
+//    @IBOutlet weak var pwdText: UITextField!
+//    @IBOutlet weak var confirmPwdText: UITextField!
+//    @IBOutlet weak var usernameText: UITextField!
+//    var segue_state:Bool = false
+//    let segueIdentifier:String = "registerSegueIdentifier"
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
 //
-
-import UIKit
-import FirebaseAuth
-
-class RegisterVC: UIViewController {
-    @IBOutlet weak var emailText: UITextField!
-    
-    @IBOutlet weak var pwdText: UITextField!
-    @IBOutlet weak var confirmPwdText: UITextField!
-    @IBOutlet weak var usernameText: UITextField!
-    var segue_state:Bool = false
-    let segueIdentifier:String = "registerSegueIdentifier"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func registerPressed(_ sender: Any) {
-        if emailText.text?.isEmpty == true || pwdText.text?.isEmpty == true {
-            segue_state = false
-            let alert = UIAlertController(title: "Missing content", message: "There are missing informatioon", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title:"OK", style:.default))
-            present(alert, animated: true)
-            
-        }
-        else if
-            pwdText.text != confirmPwdText.text{
-            segue_state = false
-            let unequalAlert = UIAlertController(title: "Passwords Unequal", message: "The two input passwords are not consistent", preferredStyle: .alert)
-            unequalAlert.addAction(UIAlertAction(title:"OK", style:.default))
-            present(unequalAlert, animated: true)
-            
-        }
-        else{
-            Auth.auth().createUser(withEmail: emailText.text!, password: pwdText.text!) {
-                authResult, error in
-                if (error as NSError?) != nil {
-                    self.segue_state = false
-                } else {
-                    self.segue_state = true
-                }
-            }
-        }
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//        // Do any additional setup after loading the view.
+//    }
+//    
+//    @IBAction func registerPressed(_ sender: Any) {
+//        if emailText.text?.isEmpty == true || pwdText.text?.isEmpty == true {
+//            segue_state = false
+//            let alert = UIAlertController(title: "Missing content", message: "There are missing informatioon", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title:"OK", style:.default))
+//            present(alert, animated: true)
+//            
+//        }
+//        else if
+//            pwdText.text != confirmPwdText.text{
+//            segue_state = false
+//            let unequalAlert = UIAlertController(title: "Passwords Unequal", message: "The two input passwords are not consistent", preferredStyle: .alert)
+//            unequalAlert.addAction(UIAlertAction(title:"OK", style:.default))
+//            present(unequalAlert, animated: true)
+//            
+//        }
+//        else{
+//            Auth.auth().createUser(withEmail: emailText.text!, password: pwdText.text!) {
+//                authResult, error in
+//                if (error as NSError?) != nil {
+//                    self.segue_state = false
+//                } else {
+//                    self.segue_state = true
+//                }
+//            }
+//        }
+//    }
+//    
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
--- BashGate/ViewController.swift
+++ BashGate/ViewController.swift
@@ -1,78 +1,78 @@
+////
+////  ViewController.swift
+////  BashGate
+////
+////  Created by Saahir Janmohamed on 10/12/22.
+////
 //
-//  ViewController.swift
-//  BashGate
+//import UIKit
+//import FirebaseAuth
+//class ViewController: UIViewController {
 //
-//  Created by Saahir Janmohamed on 10/12/22.
+//    @IBOutlet weak var emailText: UITextField!
+//    @IBOutlet weak var pwdText: UITextField!
+//    var segue_state:Bool = false
+//    let segueIdentifier:String = "loginSegueIdentifier"
+//    
+//    private let imageView: UIImageView = {
+//        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height:150))
+//        imageView.image = UIImage(named: "Subject")
+//        return imageView
+//    }()
 //
-
-import UIKit
-import FirebaseAuth
-class ViewController: UIViewController {
-
-    @IBOutlet weak var emailText: UITextField!
-    @IBOutlet weak var pwdText: UITextField!
-    var segue_state:Bool = false
-    let segueIdentifier:String = "loginSegueIdentifier"
-    
-    private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height:150))
-        imageView.image = UIImage(named: "Subject")
-        return imageView
-    }()
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(imageView)
-        // Do any additional setup after loading the view.
-    }
-    override func viewDidLayoutSubviews () {
-        super.viewDidLayoutSubviews ()
-        imageView.center = view.center
-        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: self.animate)
-        animate()
-    }
-    private func animate(){
-        UIView.animate(withDuration: 1, animations: {
-            let size = self.view.frame.size.width * 3
-            let diffX = size - self.view.frame.size.width
-            let diffY = self.view.frame.size.height - size
-            
-            self.imageView.frame = CGRect(x: -(diffX/2),
-                                          y: diffY/2,
-                                          width: size,
-                                          height: size)
-        })
-        
-        UIView.animate(withDuration: 2, animations: {
-            self.imageView.alpha = 0
-        })
-        
-    }
-    
-    @IBAction func loginPressed(_ sender: Any) {
-        if emailText.text?.isEmpty == true || pwdText.text?.isEmpty == true{
-            segue_state = false
-            let missingAlert = UIAlertController(title: "Missing content", message: "There are missing informatioon", preferredStyle: .alert)
-            missingAlert.addAction(UIAlertAction(title:"OK", style:.default))
-            present(missingAlert,animated: true)
-        }
-    else{
-        Auth.auth().signIn(withEmail: emailText.text!, password: pwdText.text!) { [self]
-            authResult, error in
-            if (error as NSError?) != nil {
-                print("error")
-                let wrongAlert = UIAlertController(title: "Warning", message: "Wrong email or password", preferredStyle: .alert)
-                wrongAlert.addAction(UIAlertAction(title:"OK", style:.default))
-                self.present(wrongAlert,animated: true)
-            } else {
-                print("in success login")
-                self.segue_state = true
-                self.performSegue(withIdentifier: segueIdentifier, sender: Any?.self)
-            }
-        }
-    }
-    }
-    
-}
-
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.addSubview(imageView)
+//        // Do any additional setup after loading the view.
+//    }
+//    override func viewDidLayoutSubviews () {
+//        super.viewDidLayoutSubviews ()
+//        imageView.center = view.center
+//        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: self.animate)
+//        animate()
+//    }
+//    private func animate(){
+//        UIView.animate(withDuration: 1, animations: {
+//            let size = self.view.frame.size.width * 3
+//            let diffX = size - self.view.frame.size.width
+//            let diffY = self.view.frame.size.height - size
+//            
+//            self.imageView.frame = CGRect(x: -(diffX/2),
+//                                          y: diffY/2,
+//                                          width: size,
+//                                          height: size)
+//        })
+//        
+//        UIView.animate(withDuration: 2, animations: {
+//            self.imageView.alpha = 0
+//        })
+//        
+//    }
+//    
+//    @IBAction func loginPressed(_ sender: Any) {
+//        if emailText.text?.isEmpty == true || pwdText.text?.isEmpty == true{
+//            segue_state = false
+//            let missingAlert = UIAlertController(title: "Missing content", message: "There are missing informatioon", preferredStyle: .alert)
+//            missingAlert.addAction(UIAlertAction(title:"OK", style:.default))
+//            present(missingAlert,animated: true)
+//        }
+//    else{
+//        Auth.auth().signIn(withEmail: emailText.text!, password: pwdText.text!) { [self]
+//            authResult, error in
+//            if (error as NSError?) != nil {
+//                print("error")
+//                let wrongAlert = UIAlertController(title: "Warning", message: "Wrong email or password", preferredStyle: .alert)
+//                wrongAlert.addAction(UIAlertAction(title:"OK", style:.default))
+//                self.present(wrongAlert,animated: true)
+//            } else {
+//                print("in success login")
+//                self.segue_state = true
+//                self.performSegue(withIdentifier: segueIdentifier, sender: Any?.self)
+//            }
+//        }
+//    }
+//    }
+//    
+//}
+//
